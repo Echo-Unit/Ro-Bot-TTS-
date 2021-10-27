d40b00fb82f6bfaaa44e34b094a92d8c14efd8e4
Rowan Yarnell <78407795+Ro-Dash@users.noreply.github.com>
2021-10-27 18:20:55 +0000

    V2 It works!

--- main.py
+++ main.py
@@ -16,11 +16,20 @@ client = discord.Client()
 
 @bot.command(alias=['tts','TTS'])
 async def tts (ctx,*, arg):
-  channel=bot.get_channel(798662468086923274)
-  await channel.send(arg)
+  channel = ctx.message.author.voice.channel
   userMessage = arg
   tts = gTTS(userMessage)
   tts.save('MSG.mp3')
+  if not channel:
+      await ctx.send("You are not connected to a voice channel")
+      return
+  voice = get(bot.voice_clients, guild=ctx.guild)
+  if voice and voice.is_connected():
+        await voice.move_to(channel)
+  else:
+        voice = await channel.connect()
+  source = FFmpegPCMAudio('MSG.mp3')
+  player = voice.play(source)
 
 @bot.command(alias=['tts','tts Repeat','tts repeat','tts rep'])
 async def botspeak(ctx):
